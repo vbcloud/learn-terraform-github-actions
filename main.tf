45d896d57e00731e021ffeddbb9b21b1c46e4df4
MookieWAF <balericaclass6@gmail.com>
2025-01-10 21:37:07 -0600

    Use our HCP Terraform organization8

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami                    = "ami-0a094c309b87cc107"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg1.id]
+  vpc_security_group_ids = [aws_security_group.web-sg8]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -62,7 +62,7 @@ resource "aws_instance" "web" {
               EOF
 }
 
-resource "aws_security_group" "web-sg1" {
+resource "aws_security_group" "web-sg8" {
   name = "${random_pet.sg.id}-sg"
   ingress {
     from_port   = 8080
