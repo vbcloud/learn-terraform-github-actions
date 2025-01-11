9f2cec40e9f4e3fb5bd36e6dfbcb5f444de63794
MookieWAF <balericaclass6@gmail.com>
2025-01-10 22:02:43 -0600

    Use our HCP Terraform organization12

--- main.tf
+++ main.tf
@@ -5,6 +5,16 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+
+
+terraform {
+    backend "s3" {
+        bucket = "mylocalterraform"
+        key = "MyLinuxBox"
+        region = "eu-west-1"      
+}
+}
+
 #4
 
 terraform {
@@ -29,62 +39,338 @@ terraform {
   }
 }
 
+resource "aws_vpc" "app1" {
+  cidr_block = "10.32.0.0/16"
 
-resource "random_pet" "sg" {}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  tags = {
+    Name = "app1"
+    Service = "application1"
+    Owner = "Chewbacca"
+    Planet = "Mustafar"
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
 }
 
+resource "aws_security_group" "app1-sg01-servers" {
+  name        = "app1-sg01-servers"
+  description = "app1-sg01-servers"
+  vpc_id      = aws_vpc.app1.id
 
-
-resource "aws_security_group" "web-sg8" {
-  name = "${random_pet.sg.id}-sg"
   ingress {
-    from_port   = 8081
-    to_port     = 8081
+    description = "MyHomePage"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "MyEvilBox"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
-#output "web-address" {
-#  value = "${aws_instance.web.public_dns}:8080"
-#}
+  tags = {
+    Name    = "app1-sg01-servers"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
 
-resource "aws_instance" "web12" {
-  ami                    = "ami-0a094c309b87cc107"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg8]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
 }
 
 
+#These are   for  public
+
+resource "aws_subnet" "public-eu-west-1a" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.32.1.0/24"
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-eu-west-1a"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+resource "aws_subnet" "public-eu-west-1b" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.32.2.0/24"
+  availability_zone       = "eu-west-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-eu-west-1b"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
 
 
+resource "aws_subnet" "public-eu-west-1c" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.32.3.0/24"
+  availability_zone       = "eu-west-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-eu-west-1c"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+#these are for private
+resource "aws_subnet" "private-eu-west-1a" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.32.11.0/24"
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name    = "private-eu-west-1a"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+resource "aws_subnet" "private-eu-west-1b" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.32.12.0/24"
+  availability_zone = "eu-west-1b"
+
+  tags = {
+    Name    = "private-eu-west-1b"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+
+resource "aws_subnet" "private-eu-west-1c" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.32.13.0/24"
+  availability_zone = "eu-west-1c"
+
+  tags = {
+    Name    = "private-eu-west-1c"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.app1.id
+
+  tags = {
+    Name    = "app1_IG"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+
+resource "aws_eip" "nat" {
+  vpc = true
+
+  tags = {
+    Name = "nat"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public-eu-west-1a.id
+
+  tags = {
+    Name = "nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.app1.id
+
+  route = [
+    {
+      cidr_block                 = "0.0.0.0/0"
+      nat_gateway_id             = aws_nat_gateway.nat.id
+      carrier_gateway_id         = ""
+      destination_prefix_list_id = ""
+      egress_only_gateway_id     = ""
+      gateway_id                 = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      local_gateway_id           = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_endpoint_id            = ""
+      vpc_peering_connection_id  = ""
+    },
+  ]
+
+  tags = {
+    Name = "private"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.app1.id
+
+  route = [
+    {
+      cidr_block                 = "0.0.0.0/0"
+      gateway_id                 = aws_internet_gateway.igw.id
+      nat_gateway_id             = ""
+      carrier_gateway_id         = ""
+      destination_prefix_list_id = ""
+      egress_only_gateway_id     = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      local_gateway_id           = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_endpoint_id            = ""
+      vpc_peering_connection_id  = ""
+    },
+  ]
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table_association" "private-eu-west-1a" {
+  subnet_id      = aws_subnet.private-eu-west-1a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private-eu-west-1b" {
+  subnet_id      = aws_subnet.private-eu-west-1b.id
+  route_table_id = aws_route_table.private.id
+}
+resource "aws_route_table_association" "private-eu-west-1c" {
+  subnet_id      = aws_subnet.private-eu-west-1c.id
+  route_table_id = aws_route_table.private.id
+}
+
+
+#public
+
+resource "aws_route_table_association" "public-eu-west-1a" {
+  subnet_id      = aws_subnet.public-eu-west-1a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-eu-west-1b" {
+  subnet_id      = aws_subnet.public-eu-west-1b.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-eu-west-1c" {
+  subnet_id      = aws_subnet.public-eu-west-1c.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+
+
+
+
+
+
+resource "aws_launch_template" "app1_LT" {
+  name_prefix   = "app1_LT"
+  image_id      = "ami-06ed60ed1369448bd"  
+  instance_type = "t2.micro"
+
+  key_name = "MyLinuxBox"
+
+  vpc_security_group_ids = [aws_security_group.app1-sg01-servers.id]
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+
+    # Get the IMDSv2 token
+    TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+    # Background the curl requests
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-ipv4 &> /tmp/local_ipv4 &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone &> /tmp/az &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/ &> /tmp/macid &
+    wait
+
+    macid=$(cat /tmp/macid)
+    local_ipv4=$(cat /tmp/local_ipv4)
+    az=$(cat /tmp/az)
+    vpc=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$macid/vpc-id)
+
+    # Create HTML file
+    cat <<-HTML > /var/www/html/index.html
+    <!doctype html>
+    <html lang="en" class="h-100">
+    <head>
+    <title>Details for EC2 instance</title>
+    </head>
+    <body>
+    <div>
+    <h1>Malgus Clan</h1>
+    <h1>Chains Broken in Ireland</h1>
+    <p><b>Instance Name:</b> $(hostname -f) </p>
+    <p><b>Instance Private Ip Address: </b> $local_ipv4</p>
+    <p><b>Availability Zone: </b> $az</p>
+    <p><b>Virtual Private Cloud (VPC):</b> $vpc</p>
+    </div>
+    </body>
+    </html>
+    HTML
+
+    # Clean up the temp files
+    rm -f /tmp/local_ipv4 /tmp/az /tmp/macid
+  EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name    = "app1_LT"
+      Service = "application1"
+      Owner   = "Chewbacca"
+      Planet  = "Mustafar"
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
